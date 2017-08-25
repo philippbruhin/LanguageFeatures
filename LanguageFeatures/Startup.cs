d05485e1ac63f6496f85383de812308f7ef2326d
philippbruhin <info@philippbruhin.ch>
2017-08-25 22:34:21 +0200

    created basic model

--- LanguageFeatures/Startup.cs
+++ LanguageFeatures/Startup.cs
@@ -12,16 +12,20 @@ namespace LanguageFeatures
 {
     public class Startup
     {
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+
+            app.UseMvcWithDefaultRoute();
 
             if (env.IsDevelopment())
             {
