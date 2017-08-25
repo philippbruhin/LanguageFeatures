ace45c6938c1ec4c29def1b616d53f38d589df71
philippbruhin <info@philippbruhin.ch>
2017-08-26 01:29:15 +0200

    combining operators in home controller

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,7 +11,15 @@ namespace LanguageFeatures.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View(new string[] { "C#", "Language", "Features" });
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                string relatedName = p?.Related?.Name;
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+            }
+            return View(results);
         }
     }
 }
